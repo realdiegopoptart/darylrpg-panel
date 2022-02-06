d336a966b24977359257ccffbcfe85d3b87a6f47
Diego Casillas <diegoboyphone@gmail.com>
2022-02-06 09:45:24 -0700

    first commit

---
+++ includes/header.php
@@ -0,0 +1,125 @@
+<head>
+    <meta charset="utf-8" />
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, maximum-scale=2, user-scalable=no"
+    />
+    <meta
+      name="description"
+      content="Semantic-UI-Forest, collection of design, themes and templates for Semantic-UI."
+    />
+    <meta name="keywords" content="Semantic-UI, Theme, Design, Template" />
+    <meta name="author" content="PPType" />
+    <meta name="theme-color" content="#ffffff" />
+    <title>Off-canvas Template for Semantic-UI</title>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
+      type="text/css"
+    />
+    <style type="text/css">
+      body {
+        padding-top: 56px;
+        -webkit-font-smoothing: antialiased;
+        -moz-font-smoothing: grayscale;
+      }
+
+      .ui.grid {
+        margin-top: 0 !important;
+      }
+
+      .ui.vertical.menu {
+        width: auto;
+      }
+
+      .ui.right.sidebar.vertical.menu {
+        width: 15rem;
+      }
+
+      .three.wide.column > .ui.vertical.menu > a.item.active,
+      .ui.right.sidebar.vertical.menu > a.item.active,
+      .three.wide.column > .ui.vertical.menu > a.item.active:hover,
+      .ui.right.sidebar.vertical.menu > a.item.active:hover {
+        background-color: #337ab7;
+        color: white;
+        border: none !important;
+      }
+
+      .three.wide.column > .ui.vertical.menu > a.item:hover,
+      .ui.right.sidebar.vertical.menu > a.item:hover {
+        background-color: #f5f5f5;
+      }
+
+      .ui.fixed.borderless.menu {
+        padding: 0;
+        flex-wrap: wrap;
+      }
+
+      .ui.fixed.borderless.menu .row > a.header.item {
+        font-size: 1.2rem;
+      }
+
+      .ui.message {
+        background-color: rgba(238, 238, 238);
+        box-shadow: none;
+        padding: 5rem 4rem;
+      }
+
+      .ui.message h1.ui.header {
+        font-size: 4.5rem;
+      }
+
+      .ui.message p.lead {
+        font-size: 1.3rem;
+        color: #333333;
+        line-height: 1.4;
+        font-weight: 300;
+      }
+
+      @media only screen and (max-width: 767px) {
+        .ui.message h1.ui.header {
+          font-size: 2rem;
+        }
+      }
+
+      button.ui.top.right.attached.button {
+        position: absolute;
+        top: 0;
+        right: 0;
+        border-bottom-right-radius: 0;
+        padding-right: 0.75rem;
+      }
+
+      .pushable {
+        height: unset;
+        overflow-x: visible;
+      }
+
+      .ui.right.sidebar {
+        height: auto !important;
+        margin-top: 1rem !important;
+        padding-left: 0;
+        padding-right: 0;
+      }
+
+      footer {
+        padding: 24px 0;
+      }
+    </style>
+  </head>
+
+  <body id="root">
+    <div class="ui grid">
+      <div class="ui top fixed borderless huge inverted menu" style="padding: 8px">
+        <div class="ui container">
+          <a class="header item">Asiania RPG</a>
+          <a class="item">Home</a> 
+          <a class="item">Assets</a>
+          <a class="item">Profile</a>
+        </div>
+        <div class="ui icon mini input">
+            <input type="text" placeholder="Search players">
+            <i class="search icon"></i>
+        </div>
+      </div>
---
+++ index.php
@@ -0,0 +1,118 @@
+<?php 
+    include_once "includes/header.php";
+?>
+
+      <div class="ui container">
+        <div class="ui padded grid">
+          <div class="thirteen wide column">
+            <div class="ui message">
+              <h1 class="ui huge header">Hello, world!</h1>
+              <p class="lead">
+                This is a simple example to show the off-canvas layout pattern
+                in Semantic-UI, click the green button to see off-canvas left
+                sidebar coming in and out of the page, and try to change the
+                viewport size to see right sidebar toggle button. You could
+                check out more wonderful effects.
+              </p>
+            </div>
+            <div class="ui hidden divider"></div>
+            <div class="ui grid">
+              <div class="three column row">
+                <div class="column">
+                  <h1 class="ui header">Heading</h1>
+                  <p>
+                    Donec id elit non mi porta gravida at eget metus. Fusce
+                    dapibus, tellus ac cursus commodo, tortor mauris condimentum
+                    nibh, ut fermentum massa justo sit amet risus. Etiam porta
+                    sem malesuada magna mollis euismod. Donec sed odio dui.
+                  </p>
+                  <button class="ui small basic button">
+                    View detailes &raquo;
+                  </button>
+                </div>
+                <div class="column">
+                  <h1 class="ui header">Heading</h1>
+                  <p>
+                    Donec id elit non mi porta gravida at eget metus. Fusce
+                    dapibus, tellus ac cursus commodo, tortor mauris condimentum
+                    nibh, ut fermentum massa justo sit amet risus. Etiam porta
+                    sem malesuada magna mollis euismod. Donec sed odio dui.
+                  </p>
+                  <button class="ui small basic button">
+                    View detailes &raquo;
+                  </button>
+                </div>
+                <div class="column">
+                  <h1 class="ui header">Heading</h1>
+                  <p>
+                    Donec id elit non mi porta gravida at eget metus. Fusce
+                    dapibus, tellus ac cursus commodo, tortor mauris condimentum
+                    nibh, ut fermentum massa justo sit amet risus. Etiam porta
+                    sem malesuada magna mollis euismod. Donec sed odio dui.
+                  </p>
+                  <button class="ui small basic button">
+                    View detailes &raquo;
+                  </button>
+                </div>
+              </div>
+              <div class="three column row">
+                <div class="column">
+                  <h1 class="ui header">Heading</h1>
+                  <p>
+                    Donec id elit non mi porta gravida at eget metus. Fusce
+                    dapibus, tellus ac cursus commodo, tortor mauris condimentum
+                    nibh, ut fermentum massa justo sit amet risus. Etiam porta
+                    sem malesuada magna mollis euismod. Donec sed odio dui.
+                  </p>
+                  <button class="ui small basic button">
+                    View detailes &raquo;
+                  </button>
+                </div>
+                <div class="column">
+                  <h1 class="ui header">Heading</h1>
+                  <p>
+                    Donec id elit non mi porta gravida at eget metus. Fusce
+                    dapibus, tellus ac cursus commodo, tortor mauris condimentum
+                    nibh, ut fermentum massa justo sit amet risus. Etiam porta
+                    sem malesuada magna mollis euismod. Donec sed odio dui.
+                  </p>
+                  <button class="ui small basic button">
+                    View detailes &raquo;
+                  </button>
+                </div>
+                <div class="column">
+                  <h1 class="ui header">Heading</h1>
+                  <p>
+                    Donec id elit non mi porta gravida at eget metus. Fusce
+                    dapibus, tellus ac cursus commodo, tortor mauris condimentum
+                    nibh, ut fermentum massa justo sit amet risus. Etiam porta
+                    sem malesuada magna mollis euismod. Donec sed odio dui.
+                  </p>
+                  <button class="ui small basic button">
+                    View detailes &raquo;
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="ui hidden divider"></div>
+            <div class="ui divider"></div>
+            <footer>Asiania RPG</footer>
+          </div>
+          <div class="three wide column">
+            <p>Online players:</p>
+            <div class="ui vertical menu">
+            <a class="item">Diego_Starm</a> <a class="item">Daryl_Reznov</a>
+            <a class="item">Xavier_Ragnarok</a> <a class="item">Jack_Addison</a>
+            <a class="item">Diego_Starm</a> <a class="item">Daryl_Reznov</a>
+            <a class="item">Xavier_Ragnarok</a> <a class="item">Jack_Addison</a>
+              <a class="item">Xavier_Ragnarok</a> <a class="item">Jack_Addison</a>
+              <a class="item">Diego_Starm</a> <a class="item">Daryl_Reznov</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+        
+<?php 
+    include_once "includes/footer.php";
+?>
