b6ca93730659eaa523d93584d81a8f5fad281de5
Diego <diegoboyphone@gmail.com>
2022-03-07 22:20:03 -0700

    remake the player.php UI

--- player.php
+++ player.php
@@ -48,94 +48,102 @@ if(isset($_GET['searchuser']))
 ?>
 
 
-  	      <div class="ui container">
-        <div class="ui padded grid">
-          <div class="sixteen wide column">
-          	
-          	<?php
-$banreasonquery = $con->prepare("SELECT * FROM banned WHERE user_id = {$sData['user_id']}");
-$banreasonquery->execute();
-$banreason = $banreasonquery->fetch();
+  	<div class="ui container">
+      <div class="ui padded grid">
+        <div class="sixteen wide column">
+          
 
-if($banreasonquery->rowCount() != 0)
-{
-    echo 
-    '<div class="ui error message">
-    	<div class="header">
-    	<i class="icon exclamation triangle"></i>
-        This player is currently banned
-    	</div>
-		    <ul class="list">
-		    <li><strong>'.$sData['user_name'].'</strong> was banned on <strong>'.$banreason['ban_date'].'</strong> for <q>'.$banreason['ban_reason'].'</q></li>
-		    </ul>
-    </div>';
-    }
+<?php
+	$banreasonquery = $con->prepare("SELECT * FROM banned WHERE user_id = {$sData['user_id']}");
+	$banreasonquery->execute();
+	$banreason = $banreasonquery->fetch();
 ?>
-            <div class="ui message">
+    <div class="ui message">
 
-  	<?php echo $sData['user_name']; ?>
-  	<div class="ui grey tiny basic horizontal label"><?php echo 'id: '.$sData['user_id']; ?></div>
+<h2 class="ui center aligned icon header">
+<?php
 
-  	<?php
-  	if($sData['admin_lvl'] > 0 || $sData['helper_level'] == 1)
-  	{
-  		$adminlvl;
+	$playerskin = $sData['skin_id'];
 
-  		if($sData['helper_level'] == 1)
-  		{
-  			$adminlvl = "Helper";
-  			break;
-  		}
+	if($sData['job_id'] != 0 || $sData['duty'] != 0)
+		$playerskin = $sJob['skin_id2'];
 
-  		switch($sData['admin_lvl']) 
-	    {
-	    	case 1:
-	            $adminlvl = "Moderator";
-	            break;
-	        case 2:
-	            $adminlvl = "Admin"; 
-	            break;
-	        case 3:
-	            $adminlvl = "Senior Admin";
-	            break;
-	        case 4:
-	            $adminlvl = "Manager";
-	            break;
-	        case 5:
-	            $adminlvl = "Server Leader";
-	            break;
-	        case 6:
-	            $adminlvl = "Director";
-	            break;
-	        case 7:
-	            $adminlvl = "Owner";
-	            break;
-	        case 8:
-	            $adminlvl = "Community Leader";
-	            break;	
-	    };
+	echo '<img class="circular icon" style="width: 75px;" src="assets/skin/heads/Skin_'.$playerskin.'.png" class="ui small centered image">';
+?>
+  <?php echo $sData['user_name']; ?>
+    <div class="ui segments">
+    <div class="ui segment">
+
+		<div class="ui grey tiny basic horizontal label"><?php echo 'id: '.$sData['user_id']; ?></div>
+	    <?php 
+	        if($sData['isonline'] == 1)
+	        {
+	            echo '<div class="ui green tiny basic horizontal label">Online</div>';
+	        }
+	        else
+	        {
+	            echo '<div class="ui red tiny basic horizontal label">Offline</div>';
+	        }
+	    ?>
+		<?php
+		if($banreasonquery->rowCount() != 0)
+		{
+		    echo 
+		    '<div class="ui red tiny basic horizontal label">banned on <strong>'.$banreason['ban_date'].' for <q>'.$banreason['ban_reason'].'</div>';
+		    }
+		?>
+
+	  	<?php
+	  	if($sData['admin_lvl'] > 0 || $sData['helper_level'] == 1)
+	  	{
+	  		$adminlvl;
+
+	  		if($sData['helper_level'] == 1)
+	  		{
+	  			$adminlvl = "Helper";
+	  			break;
+	  		}
+
+	  		switch($sData['admin_lvl']) 
+		    {
+		    	case 1:
+		            $adminlvl = "Moderator";
+		            break;
+		        case 2:
+		            $adminlvl = "Admin"; 
+		            break;
+		        case 3:
+		            $adminlvl = "Senior Admin";
+		            break;
+		        case 4:
+		            $adminlvl = "Manager";
+		            break;
+		        case 5:
+		            $adminlvl = "Server Leader";
+		            break;
+		        case 6:
+		            $adminlvl = "Director";
+		            break;
+		        case 7:
+		            $adminlvl = "Owner";
+		            break;
+		        case 8:
+		            $adminlvl = "Community Leader";
+		            break;	
+		    };
 
 
-  		echo '<div class="ui red tiny basic horizontal label">'.$adminlvl.'</div>';
-  	}
-    ?>
+	  		echo '<div class="ui red tiny basic horizontal label">'.$adminlvl.'</div>';
+	  	}
+	    ?>
 
+    </div>
+</h2>
+
+</div>
+</div>
   </div>
-	<div class="ui horizontal section divider">
-		PLAYER INFORMATION
-		</div>
-        <div class="ui padded grid">
-        <div class="ui sixteen column grid">
-        	</div>
         <div class="column">
-        <?php
-        	$playerskin = $sData['skin_id'];
-
-        	if($sData['job_id'] != 0 || $sData['duty'] != 0)
-        		$playerskin = $sJob['skin_id2'];
-
-        	echo '<img style="width: 75px;" src="assets/skin/heads/Skin_'.$playerskin.'.png" class="ui small centered image">';
-        ?>
 
             <div role="list" class="ui list">
                 <div role="listitem" class="item">
@@ -165,19 +173,19 @@ if($banreasonquery->rowCount() != 0)
                     <div class="header">Last online</div>
                     <div class="description"><?php echo $sData['last_login']; ?></div>
             	</div>
-        </div>
+        	</div>
     </div>
     <div class="column">
         <div role="list" class="ui list">
 
             <div role="listitem" class="item">
                 <div class="header">Cash</div>
-                <div class="description"> <?php echo '$'.number_format($sData['playerMoney']); ?></div>
+                <div class="description"> <?php echo '$'.number_format($sData['money']); ?></div>
             </div>
 
             <div role="listitem" class="item">
-                <div class="header">Bank Money</div>
-                <div class="description"> <?php echo '$'.number_format($sData['playerBank']); ?></div>
+                <div class="header">Phone Number</div>
+                <div class="description"> <?php echo $sData['phone_number']; ?></div>
             </div>
 
 	        <div role="listitem" class="item">
@@ -187,18 +195,17 @@ if($banreasonquery->rowCount() != 0)
 
 	        <div role="listitem" class="item">
 	            <div class="header">Drivers license</div>
-	   			<div class="description"><?php echo ($sData['playerLicense']) ? ("Yes") : ("No") ?></div>
+	   			<div class="description"><?php echo ($sData['vehicle_license']) ? ("Yes") : ("No") ?></div>
 	        </div>
 
     </div>
-</div>
 </div>
 
 	<div class="ui horizontal section divider">
 		PLAYER GROUPS
 		</div>
 <?php
-$query = $con->prepare("SELECT g.groupId, g.groupName, g.groupType, g.groupTag FROM group_member m INNER JOIN groups g ON (g.groupId = m.groupId) WHERE m.playerId = {$sData['playerId']} AND m.rankLevel != 0;");
+$query = $con->prepare("SELECT g.group_id, g.name, g.type, g.prefix FROM group_members m INNER JOIN groups g ON (g.group_id = m.groupid) WHERE m.user_id = {$sData['user_id']};");
 $query->execute();
 
 ?>
@@ -222,27 +229,32 @@ $query->execute();
 
   while($row = $query -> fetch())
   {
-      $groupId = $row['groupId'];
+      $groupId = $row['group_id'];
       $grouprankquery = $con->prepare(
-            "SELECT r.rankLevel AS level, r.rankTitle AS title 
-            FROM group_member m 
-            INNER JOIN group_ranks r ON (r.rankLevel = m.rankLevel AND r.groupId = m.groupId)
-            WHERE m.playerId = {$sData['playerId']} AND m.groupId = $groupId");
+            "SELECT r.rank_level AS level, r.rank_name AS title 
+            FROM group_members m 
+            INNER JOIN group_ranks r ON (r.rank_level = m.user_rank AND r.group_id = m.groupid)
+            WHERE m.user_id = {$sData['user_id']} AND m.groupid = $groupId");
       $grouprankquery->execute();
       $getGroupRank = $grouprankquery->fetch();
 
-        switch($row['groupType']) 
+        switch($row['type']) 
         {
             case 1:
-                $a = '<div class="ui green tiny basic horizontal label">Official</div>';
-                break;
-            case 2:
+            case 6:
+            case 9:
                 $a = '<div class="ui blue tiny basic horizontal label">Government</div>';
                 break;
-            case 3:
+            case 13:
+            case 7:
                 $a = '<div class="ui grey tiny basic horizontal label">Business</div>';
                 break;
+            case 2:
+            case 3:
             case 4:
+            case 5:
+            case 8:
+            case 12:
                 $a = '<div class="ui grey tiny basic horizontal label">Criminal</div>';
                 break;
         }
@@ -253,8 +265,8 @@ $query->execute();
             </td>
             <td class="five wide">
                 <h4 class="ui header">
-                    <div class="content"><a href="group.php?groupid='.$row['groupId'].'">'.$row['groupName'].'</a>
-                        <div class="sub header">'.$row['groupTag'].'</div>
+                    <div class="content"><a href="group.php?groupid='.$row['group_id'].'">'.$row['name'].'</a>
+                        <div class="sub header">'.$row['prefix'].'</div>
                     </div>
                 </h4>
             </td>
